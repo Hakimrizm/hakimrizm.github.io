75214fae24da81847d5d9bde28e495c24bff7fc0
Hakimrizm <hakimrizm05@gmail.com>
2024-11-19 16:41:58 +0700

    Memperbaiki Navbar ketika humburger di klik akan menambahkan class scrolled

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 const navbar = document.querySelector('.navbar');
+const submitBtn = document.querySelector('.submit')
+const navbarToggler = document.querySelector('.navbar-toggler')
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 0) {
@@ -7,3 +9,20 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('scrolled');
   }
 });
+
+navbarToggler.addEventListener("click", function() {
+  navbar.classList.add("scrolled");
+});
+
+submitBtn.addEventListener("click", function() {
+  const email = document.getElementById('email').value;
+  const nama = document.getElementById('nama').value;
+  const subject = document.getElementById('subject').value;
+  const message = document.getElementById('pesan').value;
+
+  Swal.fire({
+    title: "Good job!",
+    text: "You clicked the button!",
+    icon: "success"
+  });
+})
